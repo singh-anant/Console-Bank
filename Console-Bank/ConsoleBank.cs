217b82d0abbea2f4a3733d710a71e4506a9c4a41
anantsingh <anant.singh@mismosystems.com>
2024-07-08 06:52:05 +0530

    create a simple console login page.

---
+++ Console-Bank/ConsoleBank.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Console_Bank
+{
+	public class ConsoleBank
+	{
+		public void launchBank()
+		{
+			String userName = null;
+			String password = null;
+
+
+            Console.WriteLine("$WELCOME TO CONSOLE BANK$");
+            Console.WriteLine("Please enter your username and password:");
+			Console.Write("Username:");
+			 userName = Console.ReadLine();
+
+
+//reading the password if the username is not empty....
+			if (userName != "")
+			{
+                Console.Write("\nPassword:");
+                 password = Console.ReadLine();
+            }
+//checking hte username and password
+
+			if(userName=="System" && password=="manager")
+			{
+				System.Console.WriteLine("To display main menu");
+			}
+
+			System.Console.WriteLine("Thanks for the visit");
+            System.Console.ReadKey();
+
+
+
+        }
+    }
+}
+
--- Console-Bank/Program.cs
+++ Console-Bank/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Console_Bank;
 
+class Program
+{
+   static void Main()
+    {
+        new ConsoleBank().launchBank();
+
+    }
+}
